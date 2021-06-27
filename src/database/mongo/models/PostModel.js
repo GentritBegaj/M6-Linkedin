b50bd0d6c4f3731a6c3471e9444224ecf75e42b0
GentritBegaj <gentritbegaj@gmail.com>
2021-06-27 13:11:50 +0200

    first commit

---
+++ src/database/mongo/models/PostModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import { CommentSchema } from "./CommentModel.js";
+
+const { Schema, model } = mongoose;
+
+const postSchema = new Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    user_picture: {
+      type: String,
+      required: true,
+    },
+    comments: [CommentSchema],
+    likes: [],
+    claps: [],
+    thoughtful: [],
+    favorites: [],
+  },
+  { timestamps: true }
+);
+
+export default model("Post", postSchema);
